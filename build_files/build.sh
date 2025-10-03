9762cf35c37f9a1d476a189e2f6fa84958771262
jspurrier <45207315+jspurrier@users.noreply.github.com>
2025-10-02 20:04:44 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 #dnf5 group install -y "cosmic-desktop"
-dnf5 install -y cosmic-desktop
+dnf5 install -y tmux
 
 # Use a COPR Example:
 #
